bce6e7ddf2ff1cd953306b352481652af633db7a
CODER-nodejs <77936905+CODER-nodejs@users.noreply.github.com>
2021-07-05 16:37:25 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Body from './components/Body';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Body />
+      <header className="App-header">
+        
+      </header>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
