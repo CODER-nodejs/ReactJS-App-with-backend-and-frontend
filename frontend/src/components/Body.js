f7cbeddc2bdc7238af736908fdd79a4e12a98ec5
CODER-nodejs <77936905+CODER-nodejs@users.noreply.github.com>
2021-07-05 16:39:46 +0200

    Create Body.js

---
+++ frontend/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'; // ES6 js
+
+function Body() {
+    return (
+        <div class="myclass">
+            <h1>Hello, World!</h1>
+            <p>🎉 You made it! 🎉</p>
+        </div>
+    );
+}
+
+export default Body;
